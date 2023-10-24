c6fcf5d8215de238247d427c954a7c96c09bedc6
Bradley Rispone <bradrispone@gmail.com>
2023-10-24 13:42:46 -0500

    Add CSS spinner for while async function running

--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ let displayCelsius = true;
 let currentLocationData = {};
 
 async function fetchAndLogWeather(searchTerm) {
+
+    const loader = document.querySelector('#content .loader');
+    const dailyContent = document.getElementById('daily');
+    const weeklyContent = document.getElementById('weekly');
+
+    // hide page content and show loader
+    dailyContent.classList.toggle('hidden');
+    weeklyContent.classList.toggle('hidden');
+    loader.classList.toggle('hidden');
+    
     const weatherData = await getWeather(searchTerm);
     currentLocationData = weatherData;
     updateDOM(weatherData, displayCelsius);
+    // hide loader and show content again
+    loader.classList.toggle('hidden');
+    dailyContent.classList.toggle('hidden');
+    weeklyContent.classList.toggle('hidden');
 }
 
 fetchAndLogWeather("new york");
